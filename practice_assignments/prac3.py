15652102600c9ad9a0a867750404d86773663cde
Its-Justin <Justinramirez_2@hotmail.com>
2023-03-20 15:58:37 -0700

    Here is some of my practice assignments

---
+++ practice_assignments/prac3.py
@@ -0,0 +1,16 @@
+# Challenge 1:
+#   Fill in the missing code for the full_name function.
+#   Be sure to add the 2 parameters (and name them appropriately)
+#   Return the full name to get the desired output!
+
+def full_name(x,y):
+    # your code here!
+    name = x + y
+    return name
+name1 = full_name("Eddie ", "Aikau")
+name2 = full_name("Justin ", "Ramirez")
+print(name1) # should print Eddie Aikau
+print(name2)
+
+# Challenge 2: 
+#   Call the function again using your own name and print the results!
